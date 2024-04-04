f228654977d6c2878317e1a428141dc9092d06aa
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-04-04 22:27:32 +0100

    Initial UI commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = '';
+
+	let handleSubmit = () => {
+		console.log(name)
+  }
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>Welcome to Decyphr</h1>
+
+	<form on:submit|preventDefault={handleSubmit}>
+		<input class="form-control" type="text" placeholder="name" bind:value={name}/>
+		<button class="btn btn-primary" type="submit">Submit</button>
+	</form>
+</section>
